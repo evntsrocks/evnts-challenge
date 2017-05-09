758c8ad8fd791fb4b239c3194dee138e3f3a963e
Leandro Araújo <leandro.cesar.ma@gmail.com>
2017-05-09 16:10:57 -0300

    add lista de amenities

---
+++ src/components/ListAmenities.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './ListAmenities.css';
+
+import breakfast from '../images/breakfast.svg';
+import gym from '../images/gym.svg';
+import parking from '../images/parking.svg';
+import wifi from '../images/wifi.svg';
+
+const images = {
+  breakfast,
+  gym,
+  parking,
+  'wi-fi': wifi
+}
+
+const ListAmenities = ({amenities}) => (
+  <ul className="list-amenities">
+    {amenities.map((item, index) => {
+      const img = images[item];
+
+      return (
+        <li key={index}>
+          <img src={img} alt={item} />
+        </li>
+      )
+    })}
+  </ul>
+)
+
+export default ListAmenities;
